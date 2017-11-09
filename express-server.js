2466e9077f5f50eb3ebf3d85dbcf331978eb76c1
garima1125 <garima.arya1101@gmail.com>
2017-11-09 18:01:41 +0000

    cookies and login and logout functions implementation

--- express-server.js
+++ express-server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser")
 
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-
+app.use(cookieParser());
 var uId_length = 8;
 var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 function generateRandomString(){
@@ -40,16 +41,19 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"]};
+
   res.render("urls_index",templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"]};
+
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, long: urlDatabase[req.params.id] };
+  let templateVars = { shortURL: req.params.id, long: urlDatabase[req.params.id],username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -80,4 +84,13 @@ app.post("/urls/:id", (req,res)=> {
   res.redirect("/urls"); 
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie('username',username)
+  res.redirect("/urls");
+});
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("/urls");
+});
